578f600ff41e9cd8d1b9c0bcc23e5091de444605
Riwandi <riwandy10@gmail.com>
2024-03-18 00:41:29 +0700

    feat(form): added logic in form

--- src/components/molecules/Navigation/index.tsx
+++ src/components/molecules/Navigation/index.tsx
@@ -5,9 +5,7 @@ import { Title, Button } from "../../atoms";
 const Navigation = () => {
   return (
     <nav className="nav">
-      <div />
       <Title className="nav-title" title="DearDaily" />
-
       <Link to={"/login"}>
         <Button className="nav-login">Login</Button>
       </Link>
--- src/components/organisms/Form/index.tsx
+++ src/components/organisms/Form/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
 import { FormLogin, FormRegister, OverlayForm } from "../../molecules";
 import '../../../styles/auth/auth.css'
@@ -7,6 +7,9 @@ import '../../../styles/auth/auth.css'
 const Form = () => {
   const [toggle, setToggle] = useState(false);
   const navigate = useNavigate()
+  const location = useLocation()
+  const pathnameUrl = location.pathname
+  const checkPathname = pathnameUrl.substring(1)
 
   const signUp = () => {
     navigate('/signup')
@@ -22,6 +25,14 @@ const Form = () => {
   const signUpClasses = ["container", toggle ? "right-panel-active" : ""].join(
     " "
   );
+
+  useEffect(() => {
+    if (checkPathname === "login") {
+      setToggle(false)
+    } else {
+      setToggle(true)
+    }
+  }, [checkPathname])
 
   return (
     <div className={signUpClasses}>
